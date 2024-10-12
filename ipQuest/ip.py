3cd97f2ab16ea95e68d6ecd4b7e3d2e1c6aa7e5d
shizue <118007171+nidhi2026@users.noreply.github.com>
2024-10-12 22:45:13 +0530

    Update ip.py

--- ipQuest/ip.py
+++ ipQuest/ip.py
@@ -14,3 +14,21 @@ ip_address = requests.get('https://api.ipify.org?format=json').json()
 ipv6 = requests.get('https://api64.ipify.org?format=json').json()
 st.write(f"Your IP Address is: {ip_address['ip']}")
 st.write(f"Your IP Address is: {ipv6['ip']}")
+
+st.markdown("---")
+
+st.title("Find Your IP Address using js")
+ip_fetcher = """
+    <script>
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('ip_address').innerText = "Your IP Address is: " + data.ip;
+        })
+        .catch(error => {
+            console.error('Error fetching IP address:', error);
+        });
+    </script>
+    <div id="ip_address">Fetching your IP address...</div>
+"""
+st.markdown(ip_fetcher, unsafe_allow_html=True)
