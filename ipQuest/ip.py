51685da12c704319674890a98c7768eaaae6ba0a
shizue <118007171+nidhi2026@users.noreply.github.com>
2024-10-12 22:01:29 +0530

    Update ip.py

--- ipQuest/ip.py
+++ ipQuest/ip.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import requests
 
-def ip():
-    st.title("Find Your IP Address")
+st.title("Find Your IP Address using ipframe")
+st.markdown("""
+    <iframe src="https://api64.ipify.org?format=jsonp&callback=mycallback" 
+            style="border:none; height:50px; width:300px;"></iframe>
+""", unsafe_allow_html=True)
 
-    # Embed an iframe to display the user's IP using ipify's widget
-    st.markdown("""
-        <iframe src="https://api64.ipify.org?format=jsonp&callback=mycallback" 
-                style="border:none; height:50px; width:300px;"></iframe>
-    """, unsafe_allow_html=True)
+st.markdown("---")
+
+st.title("Find Your IP Address using ipify")
+ip_address = requests.get('https://api64.ipify.org?format=json').json()
+st.write(f"Your IP Address is: {ip_address['ip']}")
