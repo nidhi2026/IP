6fce402f8d05619421243713806ac38dd2574f89
shizue <118007171+nidhi2026@users.noreply.github.com>
2024-10-12 22:52:14 +0530

    Update ip.py

--- ipQuest/ip.py
+++ ipQuest/ip.py
@@ -6,6 +6,10 @@ st.markdown("""
     <iframe src="https://api64.ipify.org?format=jsonp&callback=mycallback" 
             style="border:none; height:50px; width:300px;"></iframe>
 """, unsafe_allow_html=True)
+st.markdown("""
+    <iframe src="https://api.ipify.org?format=jsonp&callback=mycallback" 
+            style="border:none; height:50px; width:300px;"></iframe>
+""", unsafe_allow_html=True)
 
 st.markdown("---")
 
@@ -14,22 +18,3 @@ ip_address = requests.get('https://api.ipify.org?format=json').json()
 ipv6 = requests.get('https://api64.ipify.org?format=json').json()
 st.write(f"Your IP Address is: {ip_address['ip']}")
 st.write(f"Your IP Address is: {ipv6['ip']}")
-
-st.markdown("---")
-
-st.title("Find Your IP Address using js")
-ip_fetcher = """
-    <script>
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.ip);
-            document.getElementById('ip_address').innerText = "Your IP Address is: " + data.ip;
-        })
-        .catch(error => {
-            console.error('Error fetching IP address:', error);
-        });
-    </script>
-    <div id="ip_address">Fetching your IP address...</div>
-"""
-st.markdown(ip_fetcher, unsafe_allow_html=True)
